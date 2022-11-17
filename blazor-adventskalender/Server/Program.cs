6ce1f963602180d11bba4062b9984b71519e328b
Kaiser Stefan <kaisers@ger.muehlbauer.de>
2022-11-17 20:58:27 +0100

    json testing, nothing is working

--- blazor-adventskalender/Server/Program.cs
+++ blazor-adventskalender/Server/Program.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using blazor_adventskalender.Client;
+using KristofferStrube.Blazor.FileSystemAccess;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -11,9 +15,19 @@ namespace blazor_adventskalender.Server
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            // Other services are added.
+
+            builder.Services.AddFileSystemAccessService();
+
+            await builder.Build().RunAsync();
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +36,6 @@ namespace blazor_adventskalender.Server
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
     }
 }
---
+++ blazor-adventskalender/Server/Services/JsonService.cs
@@ -0,0 +1,30 @@
+﻿using blazor_adventskalender.Client.Components;
+using blazor_adventskalender.Shared.Models;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace blazor_adventskalender.Server.Services
+{
+    public class JsonService
+    {
+        public OneDoorModel oneDoor = new();
+
+        List<OneDoorModel> doorList = new List<OneDoorModel>();
+
+        internal void ReadIn()
+        {
+            string s;
+            using (StreamReader sr = new StreamReader(@"C:\Users\kaisers\Desktop\days.json"))
+            {
+                s = sr.ReadLine();
+            }
+
+            oneDoor = JsonConvert.DeserializeObject<OneDoorModel>(s);
+            doorList.Add(oneDoor);
+
+        }
+
+    }
+}
